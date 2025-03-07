e6e087aff21c07d30895d872b3546c177fb3cc85
Jeremy Moyers <jeremy@jeremymoyers.com>
2025-03-07 12:58:02 -0800

    log connection status

--- src/ngrokServer.ts
+++ src/ngrokServer.ts
@@ -1,20 +1,29 @@
 // server-with-ngrok.ts
-import app from './app';
-import ngrok from 'ngrok'
+import app from "./app";
+import ngrok from "ngrok";
 
 const port = 3030;
 
 const startServer = async () => {
     app.listen(port, async () => {
         console.log(`Server running at http://localhost:${port}`);
-        
+
         try {
-            const url = await ngrok.connect(port);
+            const url = await ngrok.connect({
+                addr: port,
+                authtoken: process.env.NGROK_AUTHTOKEN,
+                onStatusChange: (status) => {
+                    console.log("Ngrok Status:", status);
+                },
+                onLogEvent: (log) => {
+                    console.log("Ngrok Log:", log);
+                },
+            });
             console.log(`Ngrok tunnel created at: ${url}`);
         } catch (err) {
-            console.error('Error creating ngrok tunnel:', err);
+            console.error("Error creating ngrok tunnel:", err);
         }
     });
-}
+};
 
 startServer();
